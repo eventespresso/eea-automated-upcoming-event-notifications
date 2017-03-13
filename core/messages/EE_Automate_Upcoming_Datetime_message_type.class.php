b0045c8326d67d83f683c6e22f781f2d9179d066
Darren Ethier <darren@roughsmootheng.in>
2017-03-13 11:47:22 -0400

    flesh out message type classes [touch: 10546]

--- core/messages/EE_Automate_Upcoming_Datetime_message_type.class.php
+++ core/messages/EE_Automate_Upcoming_Datetime_message_type.class.php
@@ -1,2 +1,142 @@
 <?php
-defined('EVENT_ESPRESSO') || exit('No direct access.');
+defined('EVENT_ESPRESSO_VERSION') || exit('No direct access.');
+
+/**
+ * Message type for automated upcoming event notifications.
+ * On a daily cron schedule, this message type will grab all the events where the earliest datetime is within the
+ * threshold set by the user (i.e. within x days), then send a notification based on the template generated for this
+ * message type.  No matter how many datetimes are present on the event, the notification for this message type is only
+ * sent ONCE.
+ *
+ */
+class EE_Automate_Upcoming_Datetime_message_type extends EE_Registration_Base_message_type
+{
+
+    public function __construct()
+    {
+        $this->name = 'automate_upcoming_datetime';
+        $this->description = esc_html__(
+            'This message type automates sending messages to registrations for an upcoming datetime.'
+            . 'Messages are sent at the threshold you define (eg 3 days before) prior to a datetime'
+            . ' on an event. Messages for this message type are sent to approved registrations and are'
+            . ' only triggered for datetimes on upcoming and/or sold out, and published upcoming events.'
+            . ' Note that this will send a the message for each datetime on the event.',
+            'event_espresso'
+        );
+        $this->label = array(
+            'singular' => esc_html__('automated upcoming datetime notification', 'event_espresso'),
+            'plural' => esc_html__('automated upcoming datetime notifications', 'event_espresso')
+        );
+        $this->_master_templates = array(
+            'email' => 'registration'
+        );
+        parent::__construct();
+    }
+
+
+    /**
+     * This sets up the contexts associated with the message_type
+     *
+     * @access  protected
+     * @return  void
+     */
+    protected function _set_contexts()
+    {
+        $this->_context_label = array(
+            'label' => esc_html__('recipient', 'event_espresso'),
+            'plural' => esc_html__('recipients', 'event_espresso'),
+            'description' => esc_html__(
+                'Recipient\'s are who will receive the message. There is only one message sent per attendee, no'
+                . ' no matter how many registrations are attached to that attendee.',
+                'event_espresso'
+            )
+        );
+        $this->_contexts = array(
+            'admin' => array(
+                'label' => esc_html__('Event Admin', 'event_espresso'),
+                'description' => esc_html__('This template is what event administrators will receive with an approved registration', 'event_espresso')
+            ),
+            'attendee' => array(
+                'label' => esc_html__('Registrant', 'event_espresso'),
+                'description' => esc_html__(
+                    'This is the template used to generate the message for the attendee.',
+                    'event_espresso'
+                )
+            )
+        );
+    }
+
+
+    /**
+     * @see parent::get_priority() for documentation.
+     * @return int
+     */
+    public function get_priority() {
+        return EEM_Message::priority_low;
+    }
+
+
+
+    /**
+     * Sets the data handler for this message type.
+     */
+    protected function _set_data_handler() {
+        $this->_data_handler = 'Registrations_By_Datetime';
+        //set whether this is a single message or not.
+        if (is_array($this->_data) && isset($this->_data[1]) && ! is_array($this->_data[1])) {
+            $this->_single_message = $this->_data[1] instanceof EE_Registration ? true : $this->_single_message;
+        }
+    }
+
+
+
+    /**
+     * Used to get the specific datetime if it exists in the internal $data property.
+     * Note: this will only return an EE_Datetime after the data has been setup.
+     *
+     * @return EE_Datetime|null
+     */
+    public function get_specific_datetime()
+    {
+        return isset($this->_data->specific_datetime) && $this->_data->specific_datetime instanceof EE_Datetime
+            ? $this->_data->specific_datetime
+            : null;
+    }
+
+
+    /**
+     * Called when loading the view for a specific registration for this message type and registration.
+     *
+     * @param string           $context
+     * @param \EE_Registration $registration
+     * @param int              $id  In this message type, the ID corresponds to a Datetime.
+     */
+    public function _get_data_for_context( $context, EE_Registration $registration, $id)
+    {
+        //all contexts require the $id to be set because the data handler for this message type _requires_ a datetime.
+        $datetime = EEM_Datetime::instance()->get_one_by_ID($id);
+        if (! $datetime instanceof EE_Datetime) {
+            throw new InvalidArgumentException(
+                __(
+                    'A datetime could not be retrieved for the given id. It is required for this message type.',
+                    'event_espresso'
+                )
+            );
+        }
+
+        $registrations = $context === 'admin'
+            ? EEM_Registration::instance()->get_all(
+                array(
+                    array(
+                        'Ticket.Datetime.DTT_ID' => $id
+                    ),
+                    'default_where_conditions' => EEM_Base::default_where_conditions_this_only
+                )
+            )
+            //yes this is intentionally not an array.  The format for a single registration view (which is what the
+            //registrant context is, is to have just the individual registration as the value.
+            : $registration;
+        return array($datetime, $registrations);
+    }
+
+}
--- core/messages/EE_Automate_Upcoming_Event_message_type.class.php
+++ core/messages/EE_Automate_Upcoming_Event_message_type.class.php
@@ -1,5 +1,5 @@
 <?php
-defined('EVENT_ESPRESSO') || exit('No direct access.');
+defined('EVENT_ESPRESSO_VERSION') || exit('No direct access.');
 
 /**
  * Message type for automated upcoming event notifications.
@@ -14,11 +14,26 @@ class EE_Automate_Upcoming_Event_message_type extends EE_Registration_Base_messa
 
     public function __construct()
     {
-
+        $this->name = 'automate_upcoming_event';
+        $this->description = esc_html__(
+            'This message type automates sending messages to registrations for an upcoming event.'
+            . 'Messages are sent at the threshold you define (eg 3 days before) prior to the earliest datetime attached'
+            . ' to the event.  Other datetimes on the event have no bearing on when this message type is triggered.'
+            . ' Messages for this message type are sent to approved registrations and are only triggered for upcoming'
+            . ' and/or sold out, and published upcoming events.',
+            'event_espresso'
+        );
+        $this->label = array(
+            'singular' => esc_html__('automated upcoming event notification', 'event_espresso'),
+            'plural' => esc_html__('automated upcoming event notifications', 'event_espresso')
+        );
+        $this->_master_templates = array(
+            'email' => 'registration'
+        );
+        parent::__construct();
     }
 
     /**
-     * _set_contexts
      * This sets up the contexts associated with the message_type
      *
      * @access  protected
@@ -26,6 +41,36 @@ class EE_Automate_Upcoming_Event_message_type extends EE_Registration_Base_messa
      */
     protected function _set_contexts()
     {
+        $this->_context_label = array(
+           'label' => esc_html__('recipient', 'event_espresso'),
+           'plural' => esc_html__('recipients', 'event_espresso'),
+           'description' => esc_html__(
+               'Recipient\'s are who will receive the message. There is only one message sent per attendee, no'
+               . ' no matter how many registrations are attached to that attendee.',
+               'event_espresso'
+           )
+        );
+        $this->_contexts = array(
+            'admin' => array(
+                'label' => esc_html__('Event Admin', 'event_espresso'),
+                'description' => esc_html__('This template is what event administrators will receive with an approved registration', 'event_espresso')
+            ),
+            'attendee' => array(
+               'label' => esc_html__('Registrant', 'event_espresso'),
+               'description' => esc_html__(
+                   'This is the template used to generate the message for the attendee.',
+                   'event_espresso'
+               )
+            )
+        );
+    }
 
+
+    /**
+     * @see parent::get_priority() for documentation.
+     * @return int
+     */
+    public function get_priority() {
+        return EEM_Message::priority_low;
     }
 }
