df86fd93f4477669fa5a8dc5a623e4dd6f3b4703
Darren Ethier <darren@roughsmootheng.in>
2017-03-31 21:49:00 -0400

    add php docs and fixes for bugs exposed by unit tests [touch: 10546]
    I realized that this class couldnt’ extend the exsting registrations incoming data handler because of the format of the data.

--- core/messages/EE_Messages_Registrations_By_Datetime_incoming_data.class.php
+++ core/messages/EE_Messages_Registrations_By_Datetime_incoming_data.class.php
@@ -11,7 +11,7 @@ defined('EVENT_ESPRESSO_VERSION') || exit('No direct script access allowed');
  * @author     Darren Ethier
  * @since      1.0
  */
-class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_Registrations_incoming_data
+class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_incoming_data
 {
     /**
      * @var EE_Datetime
@@ -19,13 +19,60 @@ class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_Re
     public $specific_datetime;
 
 
-
+    /**
+     * EE_Messages_Registrations_By_Datetime_incoming_data constructor.
+     *
+     * @param array $data
+     * @throws InvalidArgumentException
+     */
     public function __construct($data = array())
+    {
+        self::validate_incoming_data($data);
+        parent::__construct($data);
+    }
+
+
+    /**
+     * Returns database safe representation of the data for storage in the db.
+     *
+     * @param array  $datetime_and_registrations An array where the first element is a datetime object and the second is
+     *                                           an array of registration objects (or single registration object).
+     * @return array
+     * @throws InvalidArgumentException
+     */
+    public static function convert_data_for_persistent_storage($datetime_and_registrations)
+    {
+        self::validate_incoming_data($datetime_and_registrations);
+        //make sure $registrations are an array.
+        $datetime_and_registrations[1] = is_array($datetime_and_registrations[1])
+            ? $datetime_and_registrations[1]
+            : array($datetime_and_registrations);
+        $registration_ids = array_filter(
+            array_map(
+                function ($registration) {
+                    if ($registration instanceof EE_Registration) {
+                        return $registration->ID();
+                    }
+                    return false;
+                },
+                $datetime_and_registrations[1]
+            )
+        );
+        return array($datetime_and_registrations[0]->ID(), $registration_ids);
+    }
+
+
+    /**
+     * Validate incoming data to make sure its the expected format.
+     * @param $datetime_and_registrations
+     * @throws InvalidArgumentException
+     */
+    protected static function validate_incoming_data($datetime_and_registrations)
     {
         /**
          * is there a datetime
          */
-        if (! is_array($data) || ! reset($data) instanceof EE_Datetime) {
+        if (! is_array($datetime_and_registrations) || ! reset($datetime_and_registrations) instanceof EE_Datetime) {
             throw new InvalidArgumentException(
                 sprintf(
                     esc_html__(
@@ -41,8 +88,8 @@ class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_Re
         /**
          * is there a registration
          */
-        if ((is_array($data[1]) && ! reset($data[1]) instanceof EE_Registration)
-             || ! $data[1] instanceof EE_Registration
+        if ((is_array($datetime_and_registrations[1]) && ! reset($datetime_and_registrations[1]) instanceof EE_Registration)
+            && ! $datetime_and_registrations[1] instanceof EE_Registration
         ) {
             throw new InvalidArgumentException(
                 sprintf(
@@ -56,21 +103,6 @@ class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_Re
                 )
             );
         }
-        parent::__construct($data);
-    }
-
-
-    /**
-     * Returns database safe representation of the data for storage in the db.
-     *
-     * @param \EE_Datetime       $datetime
-     * @param \EE_Registration[] $registrations
-     * @return array
-     */
-    public static function convert_data_for_persistent_storage(EE_Datetime $datetime, $registrations)
-    {
-        $registrations =  parent::convert_data_for_persistent_storage($registrations);
-        return array($datetime->ID(), $registrations);
     }
 
 
@@ -84,8 +116,9 @@ class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_Re
      */
     public static function convert_data_from_persistent_storage($data)
     {
-        $registrations = is_array($data) && isset($data[1]) ? $data[1] : array();
-        $registrations = parent::convert_data_from_persistent_storage($registrations);
+        $registrations = is_array($data) && isset($data[1])
+            ? EEM_Registration::instance()->get_all(array(array('REG_ID' => array('IN', $data[1]))))
+            : array();
         $specific_datetime = is_array($data) && isset($data[0])
             ? EEM_Datetime::instance()->get_one_by_ID($data[0])
             : null;
@@ -120,10 +153,32 @@ class EE_Messages_Registrations_By_Datetime_incoming_data extends EE_Messages_Re
         }
 
         //now set the transaction object if possible
-        $this->txn = $this->reg_obj instanceof EE_Transaction
+        $this->txn = $this->reg_obj instanceof EE_Registration
             ? $this->reg_obj->transaction()
             : $this->_maybe_get_transaction();
 
         $this->_assemble_data();
+    }
+
+
+    /**
+     * If the incoming registrations all share the same transaction then this will return the transaction object shared
+     * among the registrations. Otherwise the transaction object is set to null because its intended to only represent
+     * one transaction.
+     *
+     * @return EE_Transaction|null
+     */
+    protected function _maybe_get_transaction()
+    {
+        $transactions = array();
+        foreach ($this->reg_objs as $registration) {
+            if ($registration instanceof EE_Registration) {
+                $transaction = $registration->transaction();
+                if ($transaction instanceof EE_Transaction) {
+                    $transactions[$transaction->ID()] = $transaction;
+                }
+            }
+        }
+        return count($transactions) === 1 ? reset($transactions) : null;
     }
 }
