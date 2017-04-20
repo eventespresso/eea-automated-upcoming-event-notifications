46ec09051d0264969ead579f1cc1d2f2ea70b05b
Darren Ethier <darren@roughsmootheng.in>
2017-04-20 14:11:05 -0400

    update bootstrap and config for tests for eea-automated-upcoming-event-notifications add-on [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,34 +1,26 @@
 <?php
 /**
- * Bootstrap for EE4 Addon Skeleton Unit Tests
- *
- * @since 		0.0.1.dev.002
- * @package 		EE4 Addon Skeleton
- * @subpackage 	Tests
+ * Bootstrap for eea-automated-upcoming-event tests
  */
 
-require( __DIR__ . '/includes/define-constants.php' );
-if ( ! is_readable( WP_TESTS_DIR . '/includes/functions.php' ) ) {
-    die( "The WordPress PHPUnit test suite could not be found.\n" );
-}
+use EETests\bootstrap\AddonLoader;
 
-require_once WP_TESTS_DIR . '/includes/functions.php';
+$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+require $core_tests_dir . 'includes/CoreLoader.php';
+require $core_tests_dir . 'includes/AddonLoader.php';
 
-function _install_and_load_core_and_waitlists() {
-    require EE_TESTS_DIR . 'includes/loader.php';
-    require EE_AUTOMATED_UPCOMING_EVENT_TESTS_DIR . 'includes/loader.php';
-}
-tests_add_filter( 'muplugins_loaded', '_install_and_load_core_and_waitlists' );
+define('EE_AUTOMATED_UPCOMING_EVENT_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EE_AUTOMATED_UPCOMING_EVENT_TESTS_DIR', EE_AUTOMATED_UPCOMING_EVENT_PLUGIN_DIR . 'tests');
 
-require WP_TESTS_DIR . '/includes/bootstrap.php';
 
-//Load the EE_specific testing tools
-require EE_TESTS_DIR . 'includes/EE_UnitTestCase.class.php';
-
-//nuke all EE4 data once the tests are done, so that it doesn't carry over to future tests.
-register_shutdown_function(
-    function() {
-        EEH_Activation::delete_all_espresso_tables_and_data();
-    }
+$addon_loader = new AddonLoader(
+    EE_AUTOMATED_UPCOMING_EVENT_TESTS_DIR,
+    EE_AUTOMATED_UPCOMING_EVENT_PLUGIN_DIR,
+    'eea-automated-upcoming-event-notification.php'
 );
-
+$addon_loader->init();
+$addon_loader->registerPsr4Path(
+    array(
+        'EventEspresso\AutomateUpcomingEventNotificationsTests' => EE_AUTOMATED_UPCOMING_EVENT_TESTS_DIR
+    )
+);
