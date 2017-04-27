a2c7b8f621e65fdbdd09004e3023b172eecb2c79
Darren Ethier <darren@roughsmootheng.in>
2017-04-27 14:40:01 -0400

    cleanup main class [touch: 10691]

--- EE_Automated_Upcoming_Event_Notification.class.php
+++ EE_Automated_Upcoming_Event_Notification.class.php
@@ -17,10 +17,9 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
 {
 
     /**
-     * @var LoaderInterface $loader;
+     * @var LoaderInterface $loader ;
      */
     private static $loader;
-
 
 
     /**
@@ -38,6 +37,7 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
 
     /**
      * Register the add-on
+     *
      * @throws \EE_Error
      */
     public static function register_addon()
@@ -46,34 +46,34 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
         EE_Register_Addon::register(
             'Automated_Upcoming_Event_Notification',
             array(
-                'version'               => Constants::version(),
-                'plugin_slug'           => 'eea_automated_upcoming_event_notifications',
-                'min_core_version'      => Constants::CORE_VERSION_REQUIRED,
-                'main_file_path'        => Constants::pluginFile(),
-                'pue_options'           => array(
+                'version'          => Constants::version(),
+                'plugin_slug'      => 'eea_automated_upcoming_event_notifications',
+                'min_core_version' => Constants::CORE_VERSION_REQUIRED,
+                'main_file_path'   => Constants::pluginFile(),
+                'pue_options'      => array(
                     'pue_plugin_slug' => 'eea-automated-upcoming-event-notifications',
                     'plugin_basename' => Constants::pluginBasename(),
                     'checkPeriod'     => '24',
                     'use_wp_update'   => false,
                 ),
-                'message_types' => array(
-                   'automate_upcoming_event' => self::get_message_type_settings(
-                       'EE_Automate_Upcoming_Event_message_type.class.php'
-                   ),
-                   'automate_upcoming_datetime' => self::get_message_type_settings(
-                       'EE_Automate_Upcoming_Datetime_message_type.class.php'
-                   )
+                'message_types'    => array(
+                    'automate_upcoming_event'    => self::get_message_type_settings(
+                        'EE_Automate_Upcoming_Event_message_type.class.php'
+                    ),
+                    'automate_upcoming_datetime' => self::get_message_type_settings(
+                        'EE_Automate_Upcoming_Datetime_message_type.class.php'
+                    ),
                 ),
-                'module_paths' => array(
+                'module_paths'     => array(
                     Constants::pluginPath()
-                        . 'domain/services/modules/EED_Automated_Upcoming_Event_Notifications.module.php',
+                    . 'domain/services/modules/EED_Automated_Upcoming_Event_Notifications.module.php',
                     Constants::pluginPath()
-                        . 'domain/services/modules/EED_Automated_Upcoming_Event_Notification_Messages.module.php',
+                    . 'domain/services/modules/EED_Automated_Upcoming_Event_Notification_Messages.module.php',
                 ),
-                'namespace' => array(
+                'namespace'        => array(
                     'FQNS' => 'EventEspresso\AutomatedUpcomingEventNotifications',
-                    'DIR' => __DIR__
-                )
+                    'DIR'  => __DIR__,
+                ),
             )
         );
     }
@@ -81,7 +81,6 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
 
     /**
      * Register things that have to happen early in loading.
-     *
      */
     public function after_registration()
     {
@@ -103,11 +102,11 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
     }
 
 
-
     /**
      * Callback for `AHEE__EE_System__load_espresso_addons__complete
      * This is also a method third party devs can use to grab the instance of this class for unsetting any hooks/actions
      * using this instance.
+     *
      * @return LoaderInterface
      */
     public static function loader()
@@ -115,25 +114,27 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
         return EE_Automated_Upcoming_Event_Notification::$loader;
     }
 
+
     /**
      * Return the settings array for the message type.
-     * @param string $mtfilename  The filename for the message type.
+     *
+     * @param string $mtfilename The filename for the message type.
      * @return array
      */
     protected static function get_message_type_settings($mtfilename)
     {
         return array(
-            'mtfilename' => $mtfilename,
-            'autoloadpaths' => array(
-                Constants::pluginPath() . 'domain/messages'
+            'mtfilename'                                       => $mtfilename,
+            'autoloadpaths'                                    => array(
+                Constants::pluginPath() . 'domain/messages',
             ),
-            'messengers_to_activate_with' => array('email'),
-            'messengers_to_validate_with' => array('email'),
-            'force_activation' => true,
+            'messengers_to_activate_with'                      => array('email'),
+            'messengers_to_validate_with'                      => array('email'),
+            'force_activation'                                 => true,
             'messengers_supporting_default_template_pack_with' => array('email'),
-            'base_path_for_default_templates' => Constants::pluginPath() . 'views/messages/templates/',
-            'base_path_for_default_variation' => Constants::pluginPath() . 'views/messages/variations/',
-            'base_url_for_default_variation' => Constants::pluginPath() . 'views/messages/variations/'
+            'base_path_for_default_templates'                  => Constants::pluginPath() . 'views/messages/templates/',
+            'base_path_for_default_variation'                  => Constants::pluginPath() . 'views/messages/variations/',
+            'base_url_for_default_variation'                   => Constants::pluginPath() . 'views/messages/variations/',
         );
     }
 
@@ -146,22 +147,22 @@ class EE_Automated_Upcoming_Event_Notification extends EE_Addon
         EE_Dependency_Map::register_dependencies(
             'EventEspresso\AutomatedUpcomingEventNotifications\domain\services\admin\Controller',
             array(
-                'EE_Request' => EE_Dependency_Map::load_from_cache
+                'EE_Request' => EE_Dependency_Map::load_from_cache,
             )
         );
         EE_Dependency_Map::register_dependencies(
             'EventEspresso\AutomatedUpcomingEventNotifications\domain\services\commands\UpcomingDatetimeNotificationsCommandHandler',
             array(
                 'EEM_Registration' => EE_Dependency_Map::load_from_cache,
-                'EEM_Datetime' => EE_Dependency_Map::load_from_cache,
-                'EE_Registry' => EE_Dependency_Map::load_from_cache
+                'EEM_Datetime'     => EE_Dependency_Map::load_from_cache,
+                'EE_Registry'      => EE_Dependency_Map::load_from_cache,
             )
         );
         EE_Dependency_Map::register_dependencies(
             'EventEspresso\AutomatedUpcomingEventNotifications\domain\services\commands\UpcomingNotificationsCommandHandler',
             array(
                 'EEM_Registration' => EE_Dependency_Map::load_from_cache,
-                'EE_Registry' => EE_Dependency_Map::load_from_cache
+                'EE_Registry'      => EE_Dependency_Map::load_from_cache,
             )
         );
     }
