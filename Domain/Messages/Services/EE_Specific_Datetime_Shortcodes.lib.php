63c9dd96bd3c2289eda48b3ef8ad034f3feccb38
Darren Ethier <darren@roughsmootheng.in>
2017-04-27 14:54:55 -0400

    cleanup class [touch: 10691]

--- Domain/Messages/Services/EE_Specific_Datetime_Shortcodes.lib.php
+++ Domain/Messages/Services/EE_Specific_Datetime_Shortcodes.lib.php
@@ -1,35 +1,53 @@
 <?php
 defined('EVENT_ESPRESSO_VERSION') || exit('No direct script access allowed');
 
+/**
+ * EE_Specific_Datetime_Shortcodes
+ * Shortcode library parser.
+ *
+ * @package EventEspresso\AutomatedUpcomingEventNotifications
+ * @author  Darren Ethier
+ * @since   1.0.0
+ */
 class EE_Specific_Datetime_Shortcodes extends EE_Shortcodes
 {
+
+    /**
+     * Initializes shortcode props.
+     */
     protected function _init_props()
     {
-        $this->label = esc_html__('Specific Datetime Shortcodes', 'event_espresso');
+        $this->label       = esc_html__('Specific Datetime Shortcodes', 'event_espresso');
         $this->description = esc_html__(
             'All shortcodes related to a specific datetime being targeted in message sending.',
             'event_espresso'
         );
         $this->_shortcodes = array(
-            '[SPECIFIC_DATETIME_START]' => esc_html__('The start date and time.', 'event_espresso'),
-            '[SPECIFIC_DATETIME_END]' => esc_html__('The end date and time.', 'event_espresso'),
+            '[SPECIFIC_DATETIME_START]'    => esc_html__('The start date and time.', 'event_espresso'),
+            '[SPECIFIC_DATETIME_END]'      => esc_html__('The end date and time.', 'event_espresso'),
             '[SPECIFIC_DATETIME_TIMEZONE]' => esc_html__('The timezone for the date and time', 'event_espresso'),
-            '[SPECIFIC_DATE_START]' => esc_html__('The datetime start date.', 'event_espresso'),
-            '[SPECIFIC_DATE_END]' => esc_html__('The datetime end date.', 'event_espresso'),
-            '[SPECIFIC_TIME_START]' => esc_html__('The datetime start time.', 'event_espresso'),
-            '[SPECIFIC_TIME_END]' => esc_html__('The datetime end time.', 'event_espresso'),
-            '[IF_DATETIME_*]' => esc_html__(
-                'A special conditional type shortcode that allows people to have content that is conditional'
-                . ' in the templates.  Note correct usage is `[IF_DATETIME_* DTT_ID=10]Content specific to when the'
-                . ' datetime has an id of 10[/IF_DATETIME_*]`. Note  you can use `preview_show` or `preview_hide` as the'
-                . ' value of DTT_ID to simulate conditional behaviour in previews when viewing templates.',
-                'event_espresso'
-            )
+            '[SPECIFIC_DATE_START]'        => esc_html__('The datetime start date.', 'event_espresso'),
+            '[SPECIFIC_DATE_END]'          => esc_html__('The datetime end date.', 'event_espresso'),
+            '[SPECIFIC_TIME_START]'        => esc_html__('The datetime start time.', 'event_espresso'),
+            '[SPECIFIC_TIME_END]'          => esc_html__('The datetime end time.', 'event_espresso'),
+            '[IF_DATETIME_*]'              => sprintf(
+                esc_html__(
+                    'A special conditional type shortcode that allows people to have content that is conditional in the templates.  Note correct usage is `%1$sContent specific to when the datetime has an id of 10%2$s`. Note  you can use `preview_show` or `preview_hide` as the value of DTT_ID to simulate conditional behaviour in previews when viewing templates.',
+                    'event_espresso'
+                ),
+                '[IF_DATETIME_* DTT_ID=10]',
+                '[/IF_DATETIME_*]'
+            ),
         );
     }
 
 
-
+    /**
+     * Parses incoming shortcode string.
+     * @param string $shortcode
+     * @return string
+     * @throws EE_Error
+     */
     protected function _parser($shortcode)
     {
         //we need a specific_datetime to do this
@@ -75,7 +93,7 @@ class EE_Specific_Datetime_Shortcodes extends EE_Shortcodes
             $show = isset($attrs['DTT_ID'])
                     && $attrs['DTT_ID'] !== 'preview_hide'
                     && (
-                        $specific_datetime->ID() === (int) $attrs['DTT_ID']
+                        $specific_datetime->ID() === (int)$attrs['DTT_ID']
                         || $attrs['DTT_ID'] === 'preview_show'
                     );
             $this->_mutate_conditional_block_in_template($shortcode, $show);
