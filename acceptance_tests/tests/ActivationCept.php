0b1b49a00d57fc689b2691892adc812d4d62608f
Darren Ethier <darren@roughsmootheng.in>
2017-05-15 21:44:17 -0400

    update activation cept [touch: 10727] [ci skip]

--- acceptance_tests/tests/ActivationCept.php
+++ acceptance_tests/tests/ActivationCept.php
@@ -1,9 +1,11 @@
 <?php
+
+use Page\CoreAdmin;
 /**
  * This test run covers the `Add-on activation` section for the critical-functionality.md checklist.
  */
 $I = new EventEspressoAddonAcceptanceTester($scenario, 'event-espresso-automated-upcoming-event-notification');
-
+$I->wantTo('Run "Add-on Activation" section for the critical-functionality.md checklist');
 $I->loginAsAdmin();
 
 //check that default templates are available in the Global Message Template list table.
@@ -12,17 +14,18 @@ $I->waitForText('Automated Upcoming Event Notification');
 $I->see('Automated Upcoming Datetime Notification');
 
 //make sure these message types have the metabox for scheduling with the two settings.
-$I->clickToEditMessageTemplateByMessageType('automate_upcoming_datetime');
+$I->click(CoreAdmin::ADMIN_LIST_TABLE_NEXT_PAGE_CLASS);
+$I->clickToEditMessageTemplateByMessageType('automate_upcoming_datetime', 'admin');
 $I->see('Scheduling Settings');
 $I->seeElement('#messages-scheduling-settings-automation-days-before');
 $I->seeElement('#messages-scheduling-settings-automation-active');
 $I->moveBack();
-$I->clickToEditMessageTemplateByMessageType('automate_upcoming_event');
+$I->clickToEditMessageTemplateByMessageType('automate_upcoming_event', 'admin');
 $I->see('Scheduling Settings');
 $I->seeElement('#messages-scheduling-settings-automation-days-before');
 $I->seeElement('#messages-scheduling-settings-automation-active');
 $I->moveBack();
-$I->clickToEditMessageTemplateByMessageType('payment');
+$I->clickToEditMessageTemplateByMessageType('payment_failed', 'primary_attendee');
 $I->dontSee('Scheduling Settings');
 
 
@@ -30,7 +33,7 @@ $I->dontSee('Scheduling Settings');
 //check that the event for the messages schedule is present.
 $I->amOnPluginsPage();
 $I->activatePlugin('wp-crontrol');
-$I->click('Cron Events');
+$I->click("//span[@class='crontrol-events']/a");
 $I->see('AHEE__EventEspresso_AutomatedEventNotifications_core_tasks_Scheduler__daily_check');
 $I->see(
     '00:00:00',
