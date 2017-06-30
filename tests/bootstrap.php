a10a700a7d58967f91dcddf5ff50098892245ba2
Brent Christensen <brent@eventespresso.com>
2017-06-30 13:10:39 -0700

    use $ee_dir  to specifiy core folder name
    use __DIR__ instead of dirname(__FILE__); [touch:10843]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,18 +2,18 @@
 /**
  * Bootstrap for eea-automated-upcoming-event tests
  */
-
+$ee_dir = 'ee4'; // 'ee4' 'event-espresso-core'
 use EETests\bootstrap\AddonLoader;
 
-$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+$core_tests_dir = dirname(dirname(__DIR__)) . "/{$ee_dir}/tests/";
 //if still don't have $core_tests_dir, then let's check tmp folder.
 if (! is_dir($core_tests_dir)) {
-    $core_tests_dir = '/tmp/event-espresso-core/tests/';
+    $core_tests_dir = "/tmp/{$ee_dir}/tests/";
 }
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
-define('EE_AUTOMATED_UPCOMING_EVENT_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EE_AUTOMATED_UPCOMING_EVENT_PLUGIN_DIR', dirname(__DIR__) . '/');
 define('EE_AUTOMATED_UPCOMING_EVENT_TESTS_DIR', EE_AUTOMATED_UPCOMING_EVENT_PLUGIN_DIR . 'tests/');
 
 
