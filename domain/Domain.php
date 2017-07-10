64ea3ef69eb1f692dd1f9dde5efa51e211695d56
Brent Christensen <brent@eventespresso.com>
2017-07-10 12:31:57 -0700

    update CORE_VERSION_REQUIRED; [touch:10843]

--- domain/Domain.php
+++ domain/Domain.php
@@ -18,7 +18,7 @@ class Domain extends DomainBase
     /**
      * EE Core Version Required for Add-on
      */
-    const CORE_VERSION_REQUIRED = '4.9.39.rc.006';
+    const CORE_VERSION_REQUIRED = '4.9.44.rc.020';
 
 
     /**
